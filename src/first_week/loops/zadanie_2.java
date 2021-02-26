e96d77a6394faee1e17f550a81bd8cd60e8316d9
MiloszEnerlich <79527936+MiloszEnerlich@users.noreply.github.com>
2021-02-26 17:56:09 +0100

    zadanie_2

---
+++ src/first_week/loops/zadanie_2.java
@@ -0,0 +1,57 @@
+package first_week.loops;
+
+import java.util.Scanner;
+
+public class zadanie_2 {
+    public static void main(String[] args) {
+        Scanner skaner = new Scanner(System.in);
+
+        System.out.println("Podaj 1. liczbę: ");
+        int a = skaner.nextInt();
+
+        System.out.println("Podaj 2. liczbę: ");
+        int b = skaner.nextInt();
+
+        System.out.println("Podaj 3. liczbę: ");
+        int c = skaner.nextInt();
+
+        if (a == b) {
+            if (a > c) {
+                System.out.println("2 pierwsze liczby są równe i większe od 3.");
+            }else{
+                System.out.println("3. liczba jest największa.");
+            }
+        } else
+            {
+            if (a > b) {
+                if (a > c) {
+                    System.out.println("1. liczba jest największa.");
+                }else{
+                    if (a==c){
+                        System.out.println("Liczby 1. i 3. są równe i większe od 2.");
+                    }else
+                    {
+                        if (c>a){
+                            System.out.println("3. liczba jest największa.");
+                        }
+                    }
+                }
+            } else
+                {
+                if (b > c) {
+                    System.out.println("2. liczba jest największa.");
+                }else{
+                    if (b==c){
+                        System.out.println("Liczby 2. i 3. są równe i większe od 1.");
+                    }else
+                    {
+                        if (c>b){
+                            System.out.println("3. liczba jest największa.");
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
