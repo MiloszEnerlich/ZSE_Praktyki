11827e134f5b0cc77c4acc99dadc3e87ceb59e1b
MiloszEnerlich <79527936+MiloszEnerlich@users.noreply.github.com>
2021-03-02 18:52:46 +0100

    zadanie_2

---
+++ src/second_week/real_loops/zadanie_2.java
@@ -0,0 +1,9 @@
+package second_week.real_loops;
+
+public class zadanie_2 {
+    public static void main(String[] args) {
+        for (int a=30; a>=0; a-=3){
+            System.out.println(a);
+        }
+    }
+}
