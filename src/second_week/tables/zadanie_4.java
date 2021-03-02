bca849502cd2e7b2c2b0d485da3e788b0ad38d5f
MiloszEnerlich <79527936+MiloszEnerlich@users.noreply.github.com>
2021-03-02 19:52:07 +0100

    zadanie_4

--- src/second_week/tables/zadanie_4.java
+++ src/second_week/tables/zadanie_4.java
@@ -6,10 +6,12 @@ import java.util.Random;
 public class zadanie_4 {
     public static void main(String[] args) {
         Random rand=new Random();
-        String[] jedzenie={"Pizza","Kurczak z ryżem","Tosty","Płatki","Nic","Lasgne","Rosół","Kanapki","Spaghetti","Jajecznica"};
-        int rand_int1= rand.nextInt(9);
-        int rand_int2= rand.nextInt(9);
+        String[] sniadanie={"Tosty","Płatki","Nic","Kanapki","Jajecznicę"};
+        String[] obiad={"Pizzę","Kurczaka z ryżem","Tosty","Nic","Lasgne","Rosół","Kanapki","Spaghetti"};
+        String[] kolacja={"Pizza","Kurczak z ryżem","Tosty","Płatki","Nic","Lasgne","Rosół","Kanapki","Spaghetti"};
+        int rand_int1= rand.nextInt(5);
+        int rand_int2= rand.nextInt(8);
         int rand_int3= rand.nextInt(9);
-        System.out.println("Na śniadanie zjem: "+jedzenie[rand_int1]+", na obiad zjem: "+jedzenie[rand_int2]+", na kolację: "+jedzenie[rand_int3]);
+        System.out.println("Na śniadanie zjem: "+sniadanie[rand_int1]+", na obiad zjem: "+obiad[rand_int2]+", na kolację: "+kolacja[rand_int3]);
     }
 }
