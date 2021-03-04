8707c4953f453518114d7d3005289a20fc3279fc
MiloszEnerlich <79527936+MiloszEnerlich@users.noreply.github.com>
2021-03-04 19:19:57 +0100

    zadanie_2

---
+++ src/second_week/multi_arrays/zadanie_2.java
@@ -0,0 +1,24 @@
+package second_week.multi_arrays;
+
+import java.util.Arrays;
+
+public class zadanie_2 {
+    public static void main(String[] args) {
+        int[][][][] arr=new int[3][3][3][3];
+        int value=0;
+        for (int i=0;i<3;i++){
+            for (int j=0;j<3;j++){
+                for (int k=0;k<3;k++){
+                    for (int l=0;l<3;l++){
+                        arr[i][j][k][l]=value;
+                        value+=2;
+                        System.out.println("["+i+"]"+"["+j+"]"+"["+k+"]"+"["+l+"] = "+arr[i][j][k][l]+ "\t");
+                    }
+                    System.out.println();
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+}
