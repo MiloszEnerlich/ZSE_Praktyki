53b41b394f510a2d6bea167851e633e51c7d4808
MiloszEnerlich <79527936+MiloszEnerlich@users.noreply.github.com>
2021-03-07 20:03:13 +0100

    Zadanie_4

---
+++ src/second_week/real_loops/zadanie_4.java
@@ -0,0 +1,11 @@
+package second_week.real_loops;
+
+public class zadanie_4 {
+    public static void main(String[] args) {
+        for (int i=-40;i<=40;i++){
+            if (i%4==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
