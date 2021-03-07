55be7ddd6e88084478493c0ba31116fbb0b48ddb
MiloszEnerlich <79527936+MiloszEnerlich@users.noreply.github.com>
2021-03-07 20:07:16 +0100

    Zadanie_5

---
+++ src/second_week/real_loops/zadanie_5.java
@@ -0,0 +1,20 @@
+package second_week.real_loops;
+
+public class zadanie_5 {
+    public static void main(String[] args) {
+
+        int a=0;
+        while (a<20){
+            System.out.println(a);
+            a+=2;
+        }
+
+        System.out.println("\n\n\n");
+
+        int b=30;
+        do{
+            System.out.println(b);
+            b-=3;
+        }while(b>0);
+    }
+}
